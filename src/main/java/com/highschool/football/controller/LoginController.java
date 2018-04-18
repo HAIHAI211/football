b6ad2f70e83d304ac7a2df8cf176ae23937eb628
harrison <851596652@qq.com>
2018-04-18 17:58:21 +0800

    加入了场馆、登录查询

--- src/main/java/com/highschool/football/controller/LoginController.java
+++ src/main/java/com/highschool/football/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.*;
 
+import com.highschool.football.exception.SessionOutOfDateException;
 import java.util.Date;
 import java.util.Optional;
 
@@ -51,16 +52,24 @@ public class LoginController {
         System.out.println("sessionId是"+sessionId);
         Session session = new Session(sessionId, openId);
         sessionRepository.save(session);
-        CommonReponse<String> response = new CommonReponse<>(CommonReponse.SUCCESS_CODE, CommonReponse.SUCCESS_MSG, sessionId);
-        return response;
+        return new CommonReponse(CommonReponse.SUCCESS_CODE, CommonReponse.SUCCESS_MSG, sessionId);
     }
 
     /*
     * 登录 session
     * */
-    @GetMapping(value="/s/{sessionKey}")
-    private void loginWithSession(@PathVariable String sessionKey){
-
-
+    @GetMapping(value="/s/{sessionId}")
+    private CommonReponse<String> loginWithSession(@PathVariable String sessionId){
+        //根据sessionId查询session是否存在且未过期
+        Date now = new Date();
+        Date lastDate = DateUtil.dateAddHours(now, 1);
+        Optional<Session> sessionOptional = sessionRepository.findFirstBySessionIdAndLastDateAfter(sessionId, now);
+        if (sessionOptional.isPresent()) { // 如果存在
+            Session session = sessionOptional.get();
+            session.setLastDate(lastDate);
+            return new CommonReponse(CommonReponse.SUCCESS_CODE, CommonReponse.SUCCESS_MSG, sessionId);
+        } else { // 如果不存在
+            return new CommonReponse(CommonReponse.LOGIN_FAIL_CODE, CommonReponse.LOGIN_FAIL_MSG, sessionId);
+        }
     }
 }
